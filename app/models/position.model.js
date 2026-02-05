9593974fe688bcef07ebce40ea632cc46b4efb37
parkerlandes <parker.landes@eagles.oc.edu>
2026-02-05 12:47:50 -0600

    add Model for Position table

---
+++ app/models/position.model.js
@@ -0,0 +1,37 @@
+import Sequelize from "sequelize";
+import SequelizeInstance from "../config/sequelizeInstance";
+
+const Task = SequelizeInstance.define("position", {
+
+    //Primary Key
+    id_position: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    avgPayRate:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: "Short Desciption of task to be completed...",
+    },
+    
+    id_area: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+    },
+
+    //foreign key
+    id_department:{
+        type: Sequelize.INTEGER,
+        foreignKey: true,
+    },
+
+});
+
+export default Task;
