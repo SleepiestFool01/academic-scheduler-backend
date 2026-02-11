29145642a2b24854b39563706cc385df56b14cdc
SleepiestFool01 <teaganm856@gmail.com>
2026-02-11 12:28:45 -0600

    Employee Controller, and updating service file to match deployment

--- app/models/employee.model.js
+++ app/models/employee.model.js
@@ -10,6 +10,12 @@ const Employee = SequelizeInstance.define("employee", {
         primaryKey: true,
     },
 
+    role: {
+        type: Sequelize.ENUM("Employee", "Manager", "Admin"),
+        allowNull: false,
+        defaultValue: "Employee",   
+    },
+
     fName: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -27,12 +33,6 @@ const Employee = SequelizeInstance.define("employee", {
         allowNull: true,
         defaultValue: "Share your goals, experience, or anything your coach should know.",
     },
-    role: {
-        type: Sequelize.ENUM("Employee", "Manager", "Admin"),
-        allowNull: false,
-        defaultValue: "Employee",   
-    }
-
 });
 
 export default Employee;
