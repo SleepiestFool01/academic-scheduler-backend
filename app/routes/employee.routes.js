7db6d832a20e2aa50aaaf060cbb72c7caf7fe011
SleepiestFool01 <teaganm856@gmail.com>
2026-02-11 12:39:46 -0600

    Employees Route

---
+++ app/routes/employee.routes.js
@@ -0,0 +1,30 @@
+import users from "../controllers/employee.controller.js";
+import authenticate from "../authorization/authorization.js";
+import { Router } from "express";
+var router = Router()
+
+// Create a new Employee
+router.post("/", [authenticate], employees.create);
+
+// Create a new Emloyee with 
+router.post("/create-employee", [authenticate], employees.createEmployee);
+
+// Retrieve all People/Employees
+router.get("/", [authenticate], employees.findAll);
+
+// Retrieve all employees
+router.get("/employees", [authenticate], employees.findAllEmployees);
+
+// Retrieve a single Employee with id_employee
+router.get("/:id_employee", [authenticate], employees.findOne);
+
+// Update a Employee with id_employee
+router.put("/:id_employee", [authenticate], employees.update);
+
+// Update Employee's Role
+router.put("/role/:id_employee", [authenticate], employees.updateRole);
+
+// Delete a Employee with id_employee
+router.delete("/:id_employee", [authenticate], employees.delete);
+
+export default router;
