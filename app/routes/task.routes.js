20ce1b449ccced6fefadf279b76fd2d0ec92461e
Ryan Allen <ryan.allen@eagles.oc.edu>
2026-02-17 17:43:39 -0600

    Started work on task route

---
+++ app/routes/task.routes.js
@@ -0,0 +1,31 @@
+import task from "../controllers/task.controller.js";
+import authenticate from "../authorization/authorization.js";
+import { Router } from "express";
+
+var router = Router()
+
+// Create a new Task
+router.post("/", [authenticate], task.create);
+
+// Create a new Emloyee with 
+router.post("/create-employee", [authenticate], employees.createEmployee);
+
+// Retrieve all People/Employees
+router.get("/", [authenticate], employees.findAll);
+
+// Retrieve all employees
+router.get("/employees", [authenticate], employees.findAllEmployees);
+
+// Retrieve a single Employee with id_employee
+router.get("/:id_employee", [authenticate], employees.findOne);
+
+// Update a Employee with id_employee
+router.put("/:id_employee", [authenticate], employees.update);
+
+// Update Employee's Role
+router.put("/role/:id_employee", [authenticate], employees.updateRole);
+
+// Delete a Employee with id_employee
+router.delete("/:id_employee", [authenticate], employees.delete);
+
+export default router;
